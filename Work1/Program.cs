2f87f1b38a4e31115075b75f36d2ea9381480821
Ustyugova Julia <bublikjulija@rambler.ru>
2019-07-28 20:05:34 +0600

    commit

---
+++ Work1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int i = rand.Next(100);
+            var count = 1;
+
+            while (true)
+            {
+                //Console.WriteLine("Введите " + count + " число:");
+                Console.WriteLine($"Введите {count} число");
+                count++;
+                int k = Convert.ToInt32(Console.ReadLine());
+
+                if (i == k)
+                {
+                    Console.WriteLine("Успех");
+                    break;
+                }
+
+                if (i > k)
+                {
+                    Console.WriteLine("Горячо");
+                }
+
+                if (i < k)
+                {
+                    Console.WriteLine("Холодно");
+                }
+
+            }
+        }
+    }
+}
